b94ba25aed06f82e5ebf20e1f0b1bf879b45d91b
TerhiElina <terhi.evakallio@gmail.com>
2024-05-15 11:12:51 +0300

    Deployment yritys

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/DeltaKyselyFront/', 
   plugins: [react()],
+  build: {
+    outDir: 'dist', // This is the default output directory
+  },
 })
